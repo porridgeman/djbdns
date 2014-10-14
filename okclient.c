1a22aebe428f422fa60e4ef2f16bde9b4a54f209
Roland Mechler <rmechler@gmail.com>
2014-10-14 16:24:22 -0700

    first stab at caching access control lookup result

--- okclient.c
+++ okclient.c
@@ -3,10 +3,16 @@
 #include "str.h"
 #include "ip4.h"
 #include "okclient.h"
+#include "cache.h"
+
+#define CACHESIZE 1000000
+#define TTL 1800 // TODO is this in seconds?
+static int initialized = 0;
+static cache_t cache;
 
 static char fn[3 + IP4_FMT];
 
-int okclient(char ip[4])
+int match_file(char ip[4])
 {
   struct stat st;
   int i;
@@ -23,4 +29,29 @@ int okclient(char ip[4])
     if (!fn[i]) return 0;
     fn[i] = 0;
   }
+}
+
+int okclient(char ip[4])
+{
+  char *cached;
+  unsigned int cachedlen;
+  uint32 ttl;
+  int result;
+  char data;
+
+  if (!initialized) {
+    cache = cache_t_new(CACHESIZE);
+    initialized = 1;
+  }
+
+  cached = cache_t_get(cache,ip,4,&cachedlen,&ttl);
+  if (cached) {
+    result = *cached;
+  } else {
+    result = match_file(ip);
+    data = result;
+    cache_t_set(cache,ip,4,&data,sizeof(data),TTL);
+  }
+
+  return result;
 }
